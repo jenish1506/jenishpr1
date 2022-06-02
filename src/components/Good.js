f10360c06e803846b6c8dab07429a29b5f249d72
jenish <you@example.com>
2022-06-02 15:28:29 +0530

    hgjhgh

---
+++ src/components/Good.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Good extends Component {
+    render() {
+        return (
+            <div>
+                
+                    jenish gondaliya
+            </div>
+        );
+    }
+}
+
+export default Good;
